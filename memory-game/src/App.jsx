3b3a27fd877e6876c13c7646e6b2cbf0303113ff
dmychel <dmychel@tutanota.com>
2023-12-30 11:15:12 -0500

    Call API and shuffle
    Call API slightly moddified to retrive mulitple of the same images. Once all the fetch requests have been completed it calls the shuffleArray function that rearranges the arraya and then sets the entry useState with the updated array.

--- memory-game/src/App.jsx
+++ memory-game/src/App.jsx
@@ -1,5 +1,7 @@
 import "./App.css";
+import Cards from "./components/Cards";
 import Header from "./components/Header";
+import Score from "./components/Score";
 import Start from "./components/Start";
 import { useState } from "react";
 
@@ -12,24 +14,47 @@ function App() {
   const callAPI = async (difficulty) => {
     let arr = [];
     if (difficulty === 2) {
-      for (let i = 0; i < 5; i++) {
+      for (let i = 0; i < 8; i++) {
         const res = await fetch(
           `${BASE_URL}/${Math.floor(Math.random() * 380)}`
         );
         const data = await res.json();
         const url = await data.data.image;
         arr.push(url);
+        arr.push(url);
       }
-      setEntry(arr);
-      console.log("done");
+      shuffleArray(arr);
+    } else {
+      for (let i = 0; i < 3; i++) {
+        const res = await fetch(
+          `${BASE_URL}/${Math.floor(Math.random() * 380)}`
+        );
+        const data = await res.json();
+        const url = await data.data.image;
+        arr.push(url);
+        arr.push(url);
+        arr.push(url);
+      }
+      shuffleArray(arr);
     }
+  };
+
+  const shuffleArray = (arr) => {
+    let newArr = arr
+      .map((value) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value);
+
+    setEntry(newArr);
   };
 
   return (
     <>
       <section className="app">
         <Header />
+        <Score />
         <Start callAPI={callAPI} entry={entry} BASE_URL={BASE_URL} />
+        <Cards entry={entry} />
       </section>
     </>
   );
