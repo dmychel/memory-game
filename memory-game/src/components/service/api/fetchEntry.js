d4beb23a712bc813cfb44ef40cce5fb88f1c5cbb
dmychel <dmychel@tutanota.com>
2023-12-31 00:36:09 -0500

    fetch api component template

---
+++ memory-game/src/components/service/api/fetchEntry.js
@@ -0,0 +1,10 @@
+export const fetchEntry = async (entry) => {
+    const BASE_URL =
+        "https://botw-compendium.herokuapp.com/api/v3/compendium/entry";
+    const res = await fetch(
+        `${BASE_URL}/${Math.floor(Math.random() * entry)}`
+    );
+    const data = await res.json();
+    const url = await data.data.image;
+    return url
+}
