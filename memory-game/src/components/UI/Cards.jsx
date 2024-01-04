13018b0d51b68ac6bcefcc9ba4477f991bd0addf
dmychel <dmychel@tutanota.com>
2024-01-04 15:12:46 -0500

    removed jsx and replaced it with a component

--- memory-game/src/components/UI/Cards.jsx
+++ memory-game/src/components/UI/Cards.jsx
@@ -1,22 +1,29 @@
 import "../../styles/card.css";
 import PropTypes from "prop-types";
+import Card from "./primitives/Card";
 
-const Cards = ({ tiles }) => {
+const Cards = ({ tiles, selection, setSelection }) => {
   return (
     <>
       <section className="card-master">
         {tiles.map((url) => (
-          <div className="card-container" key={crypto.randomUUID()}>
-            {console.log(tiles)}
-            <img src={url} alt="try again" className="card" />
-          </div>
+          <Card
+            key={crypto.randomUUID()}
+            url={url.image}
+            selection={selection}
+            setSelection={setSelection}
+            data={url}
+          />
         ))}
       </section>
     </>
   );
 };
+
 export default Cards;
 
 Cards.propTypes = {
   tiles: PropTypes.array,
+  selection: PropTypes.array,
+  setSelection: PropTypes.func,
 };
