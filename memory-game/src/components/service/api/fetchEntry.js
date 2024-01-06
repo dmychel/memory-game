6abfd8573067111339cecccf7b49cda2756a7b0d
dmychel <dmychel@tutanota.com>
2024-01-05 22:53:05 -0500

    added unique id to each object

--- memory-game/src/components/service/api/fetchEntry.js
+++ memory-game/src/components/service/api/fetchEntry.js
@@ -1,10 +1,9 @@
 export const fetchEntry = async (entry) => {
-    const BASE_URL =
-        "https://botw-compendium.herokuapp.com/api/v3/compendium/entry";
-    const res = await fetch(
-        `${BASE_URL}/${Math.floor(Math.random() * entry)}`
-    );
-    const data = await res.json();
-    const url = await data.data
-    return url
-}
+  const BASE_URL =
+    "https://botw-compendium.herokuapp.com/api/v3/compendium/entry";
+  const res = await fetch(`${BASE_URL}/${Math.floor(Math.random() * entry)}`);
+  const data = await res.json();
+  const url = await data.data;
+  url.uniqueID = crypto.randomUUID();
+  return url;
+};
