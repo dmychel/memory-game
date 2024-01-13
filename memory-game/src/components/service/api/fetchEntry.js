f6ea39b2c0f872a93e34f41eab9971a580e99a2e
dmychel <dmychel@tutanota.com>
2024-01-13 00:11:41 -0500

    try catch statement

--- memory-game/src/components/service/api/fetchEntry.js
+++ memory-game/src/components/service/api/fetchEntry.js
@@ -1,8 +1,13 @@
 export const fetchEntry = async (entry) => {
   const BASE_URL =
     "https://botw-compendium.herokuapp.com/api/v3/compendium/entry";
-  const res = await fetch(`${BASE_URL}/${Math.floor(Math.random() * entry)}`);
-  const data = await res.json();
-  const url = await data.data;
-  return url;
+  try {
+    const res = await fetch(`${BASE_URL}/${Math.floor(Math.random() * entry)}`);
+    const data = await res.json();
+    const url = await data.data;
+    return url
+  } catch (err) {
+    console.log(err)
+    alert('Please Refresh the page and try again.')
+  }
 };
