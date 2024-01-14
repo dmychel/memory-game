1997061733a9e05c37a974f7a8f36870d2133b54
dmychel <dmychel@tutanota.com>
2024-01-14 18:38:13 -0500

    try again button

--- memory-game/src/components/UI/primitives/GameOver.jsx
+++ memory-game/src/components/UI/primitives/GameOver.jsx
@@ -4,6 +4,9 @@ const GameOver = () => {
   return (
     <>
       <h1>Game Over</h1>
+      <button className="game-over-btn" onClick={() => location.reload()}>
+        Try Again
+      </button>
     </>
   );
 };
