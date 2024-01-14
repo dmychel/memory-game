580d106be7906b63dab6d41f2bcc923bf2a758a6
dmychel <dmychel@tutanota.com>
2024-01-14 17:37:29 -0500

    components
    created new components for hard mode. Due to lack of space on the page, the hard mode components return just a card with an image, rather than an image and a title. The class name is also dynamically added to each component via a function.

--- memory-game/src/components/UI/Card.jsx
+++ memory-game/src/components/UI/Card.jsx
@@ -1,32 +1,75 @@
 import PropTypes from "prop-types";
 
-const Card = ({ index, data, url, handleFlip, selection, setSelection }) => {
+const Card = ({
+  index,
+  data,
+  url,
+  handleFlip,
+  selection,
+  setSelection,
+  difficulty,
+}) => {
   const handleClick = () => {
     return setSelection([...selection, data.id]), handleFlip(index);
   };
 
+  const regClass = () => {
+    if (difficulty === 1) {
+      return "card";
+    }
+    return "hard-card";
+  };
+
+  const flippedClass = () => {
+    if (difficulty === 1) {
+      return "card-flipped";
+    }
+    return "hard-card-flipped";
+  };
+
+  const RegCard = () => {
+    return (
+      <div className={regClass()} onClick={() => handleClick()}>
+        <img src="../../src/assets/img/card-art.png" alt="Card Art" />
+      </div>
+    );
+  };
+
   const FlippedCard = () => {
     return (
-      <div className="card-flipped">
+      <div className={flippedClass()} onClick={() => handleClick()}>
         <img src={url} alt="Card" />
         <p>{data.name}</p>
       </div>
     );
   };
 
-  const RegCard = () => {
+  const RegCardHard = () => {
     return (
-      <div className="card" onClick={() => handleClick()}>
+      <div className={regClass()} onClick={() => handleClick()}>
         <img src="../../src/assets/img/card-art.png" alt="Card Art" />
       </div>
     );
   };
 
+  const FlippedCardHard = () => {
+    return (
+      <div className={flippedClass()} onClick={() => handleClick()}>
+        <img src={url} alt="Card" />
+      </div>
+    );
+  };
+
+  const cardType = () => {
+    if (difficulty === 1) {
+      return data.flipped ? <FlippedCard /> : <RegCard />;
+    }
+    return data.flipped ? <FlippedCardHard /> : <RegCardHard />;
+  };
+
   return (
     <>
-      <div className="card-container">
-        {data.flipped ? <FlippedCard /> : <RegCard />}
-      </div>
+      <div className="card-container">{cardType()}</div>
     </>
   );
 };
@@ -40,4 +83,5 @@ Card.propTypes = {
   data: PropTypes.object,
   handleFlip: PropTypes.func,
   id: PropTypes.string,
+  difficulty: PropTypes.number,
 };
