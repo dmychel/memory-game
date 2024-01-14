97d20fefd34328abe636c580b0d79ee4ff2a16d0
dmychel <dmychel@tutanota.com>
2024-01-14 18:20:43 -0500

    moved logic to app.jsx

--- memory-game/src/components/UI/Start.jsx
+++ memory-game/src/components/UI/Start.jsx
@@ -1,38 +1,19 @@
 import "../../styles/start.css";
 import Button from "./primitives/Button";
-import { fetchEntry } from "../service/api/fetchEntry";
 import PropTypes from "prop-types";
 
-const Start = ({ setTiles, gameStart, setGameStart, setDifficulty }) => {
-  const removeDislpay = () => {
-    const htmlEL = (document.getElementById("startMas").style.display = "none");
-  };
-
-  const startGame = async (difficulty) => {
-    const arrPhase1 = await callAPI(difficulty);
-    const arrPhase2 = addUniqueID(arrPhase1);
-    const arrPhase3 = shuffle(arrPhase2);
-
-    removeDislpay();
-    setGameStart(!gameStart);
-    setDifficulty(difficulty);
-    setTiles(arrPhase3);
-
-    setTimeout(() => {
-      const phaseFinal = [...arrPhase3];
-      phaseFinal.map((obj) => {
-        return (obj.flipped = false);
-      });
-      setTiles(phaseFinal);
-    }, "3000");
+const Start = ({ startGame }) => {
+  const initiateGame = (value) => {
+    const htmlEL = document.getElementById("startMas");
+    return startGame(value), (htmlEL.style.display = "none");
   };
 
   return (
     <>
       <section className="start-master" id="startMas">
         <div className="btn-container">
-          <Button title={"Easy"} clickEvent={() => startGame(1)} />
-          <Button title={"Hard"} clickEvent={() => startGame(2)} />
+          <Button title={"Easy"} clickEvent={() => initiateGame(1)} />
+          <Button title={"Hard"} clickEvent={() => initiateGame(2)} />
         </div>
       </section>
     </>
@@ -41,55 +22,10 @@ const Start = ({ setTiles, gameStart, setGameStart, setDifficulty }) => {
 
 export default Start;
 
-const callAPI = async (difficulty) => {
-  let arr = [];
-  let num = Math.floor(Math.random() * 380);
-
-  if (difficulty === 1) {
-    for (let i = 0; i < 3; i++) {
-      const promise = await fetchEntry(num);
-
-      const res1 = { ...promise };
-      const res2 = { ...promise };
-      const res3 = { ...promise };
-
-      arr.push(res1, res2, res3);
-    }
-    return arr;
-  } else {
-    let num = Math.floor(Math.random() * 380);
-    for (let i = 0; i < 8; i++) {
-      const promise = await fetchEntry(num);
-
-      const res1 = { ...promise };
-      const res2 = { ...promise };
-
-      arr.push(res1, res2);
-    }
-    return arr;
-  }
-};
-
-const addUniqueID = (arr) => {
-  let newArr = arr;
-  newArr.map((value) => {
-    return (value.uniqueID = crypto.randomUUID());
-  });
-  return newArr;
-};
-
-// shuffle card order
-const shuffle = (arr) => {
-  let newArr = arr
-    .map((value) => ({ value, sort: Math.random() }))
-    .sort((a, b) => a.sort - b.sort)
-    .map(({ value }) => value);
-  return newArr;
-};
-
 Start.propTypes = {
   setTiles: PropTypes.func,
   gameStart: PropTypes.bool,
   setGameStart: PropTypes.func,
   setDifficulty: PropTypes.func,
+  startGame: PropTypes.func,
 };
