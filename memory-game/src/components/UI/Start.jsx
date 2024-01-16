7b6ded378c2c6caeb906f669a723eda61469445f
dmychel <dmychel@tutanota.com>
2024-01-15 23:41:36 -0500

    added h2

--- memory-game/src/components/UI/Start.jsx
+++ memory-game/src/components/UI/Start.jsx
@@ -11,6 +11,9 @@ const Start = ({ startGame }) => {
   return (
     <>
       <section className="start-master" id="startMas">
+        <div className="title">
+          <h2>Select a Diffculty</h2>
+        </div>
         <div className="btn-container">
           <Button title={"Easy"} clickEvent={() => initiateGame(1)} />
           <Button title={"Hard"} clickEvent={() => initiateGame(2)} />
